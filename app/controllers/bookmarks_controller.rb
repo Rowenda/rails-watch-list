95741b6d27bfcbf7fd2a86035aa9b1c1399933df
baptiste giorgi <badbat_69@hotmail.com>
2021-12-09 17:02:16 +0100

    rake completed

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,6 +7,8 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(set_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@bookmark.list)
     else
